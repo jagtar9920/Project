73e4a30913491b2cecdfc3b6a1a3aaa8da8dd2c4
jagtar9920 <55209501+jagtar9920@users.noreply.github.com>
2021-04-20 22:44:55 -0400

    Update num.py

--- num.py
+++ num.py
@@ -1,27 +1,6 @@
-# #Three lines to make our compiler able to draw:
-# import sys
-# import matplotlib
 
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# xpoints = np.array([0, 6])
-# ypoints = np.array([0, 500])
-
-# plt.xlabel("Weeks")
-# plt.ylabel("Covid Case")
-
-# plt.plot(xpoints, ypoints)
-# plt.show()
-
-
-
-# #Two  lines to make our compiler able to draw:
-# plt.savefig(sys.stdout.buffer)
-# sys.stdout.flush()
-
-#Created by Amarjot Singh
+#Created by Jagtar Singh
 import matplotlib.pyplot as plt
 import numpy as np
 
